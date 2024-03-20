f91f4ffec6429703d80bb8c6fb5c2f744aa0a95d
gilbertmunuotz <officialgilbert24@gmail.com>
2024-03-19 21:57:24 -0700

    little stuff.

--- src/components/Cat3.jsx
+++ src/components/Cat3.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Navbar from "./Navbar";
+import Footer from './Footer'
 
 function Cat3() {
     return (
         <div className="Cat3">
             <Navbar />
-            <p className="text-base">
-                Our Listed Products are 100% Original and comes with 2 Month Warranty Period. With VAT included.
+            <p className="mt-20 text-lg mx-12">
+                Our Listed Products are 100% Original and comes with 2 Month Warranty Period, with VAT included.
             </p>
 
-            <h1 className="text-3xl text-center">Product Category 3</h1>
+            <h1 className="text-3xl text-center my-10">Product Category</h1>
+            <Footer />
         </div>
     )
 }
