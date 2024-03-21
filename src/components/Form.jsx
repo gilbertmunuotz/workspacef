e8f79a9d270deb88d3dca52568e751aae970b741
gilbertmunuotz <officialgilbert24@gmail.com>
2024-03-21 08:11:59 -0700

    Minor updates....

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -14,7 +14,7 @@ function Form() {
     function HandleSubmit(event) {
         event.preventDefault();
 
-        const url = 'http://localhost:3001/api/send';
+        const url = 'https://workspaceb.vercel.app/send';
         const UserData = { name, message, email };
 
         const requestOptions = {
