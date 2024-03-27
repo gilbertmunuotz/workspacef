fa47d4a7f1c595330de307f067947d0cefa16c35
gilbertmunuotz <officialgilbert24@gmail.com>
2024-03-26 18:37:00 -0700

    Revert "finished..."
    This reverts commit 1b64232e697d47dd07004fd4e5053dd7c216f990.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,7 +15,6 @@ function Form() {
         event.preventDefault();
 
         const url = 'https://workspaceb.vercel.app/api/users';
-        
         const UserData = { name, email, message };
 
         const requestOptions = {
